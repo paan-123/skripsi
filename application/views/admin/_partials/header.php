15dac8bb29660fe4decb55fcc6c2cce9aa7f5e83
Bunnys666 <muhammadryanapriyudan@gmail.com>
2020-10-06 11:20:59 -0700

    templating

---
+++ application/views/admin/_partials/header.php
@@ -0,0 +1,14 @@
+<div class="header-mobile__bar">
+                <div class="container-fluid">
+                    <div class="header-mobile-inner">
+                        <a class="logo" href="index.html">
+                            <img src="<?php echo base_url('assets/assets-admin/images/icon/logo.png');?>" alt="CoolAdmin" />
+                        </a>
+                        <button class="hamburger hamburger--slider" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            
